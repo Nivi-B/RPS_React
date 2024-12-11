bec5f5fb42560677c022d8e409a7a096d2c93fbe
Nivi-B <113551675+Nivi-B@users.noreply.github.com>
2024-12-11 04:54:26 -0500

    project setup

---
+++ src/RockPaperScissors.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RockPaperScissors = () => {
+  return (
+    <div>
+      <h1>Rock Paper Scissors</h1>
+      <p>Player name and game logic will go here.</p>
+    </div>
+  );
+};
+
+export default RockPaperScissors;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import RockPaperScissors from './RockPaperScissors';
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<RockPaperScissors />, rootElement);
